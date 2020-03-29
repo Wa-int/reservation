a73e6464f7903dc7403b2ca942c6173a71640c76
Wa-int <waint@odds.team>
2020-03-30 02:32:25 +0700

    Updates

---
+++ precache-manifest.6356d0611032b31278af8db7e96acc39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0060a6e8179e933439cca2f005aed7",
+    "url": "/home/reservations/index.html"
+  },
+  {
+    "revision": "395b840abba6f0d203d6",
+    "url": "/home/reservations/static/css/2.6df9c2a8.chunk.css"
+  },
+  {
+    "revision": "064ce3624a690afb22b6",
+    "url": "/home/reservations/static/css/main.79816ffe.chunk.css"
+  },
+  {
+    "revision": "395b840abba6f0d203d6",
+    "url": "/home/reservations/static/js/2.df354463.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/reservations/static/js/2.df354463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064ce3624a690afb22b6",
+    "url": "/home/reservations/static/js/main.5db8b50b.chunk.js"
+  },
+  {
+    "revision": "1ecc1046ab68bae90993",
+    "url": "/home/reservations/static/js/runtime-main.1954fc9d.js"
+  }
+]);
