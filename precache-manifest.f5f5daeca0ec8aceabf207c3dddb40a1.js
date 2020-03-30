7a3228fa2a34498b54316745f760d8e5ead22adb
Wa-int <waint@odds.team>
2020-03-31 01:58:25 +0700

    Updates

---
+++ precache-manifest.f5f5daeca0ec8aceabf207c3dddb40a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd801f501430616cd9f6e3232029fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d76f39c62765e85400d",
+    "url": "/static/css/2.fa503aff.chunk.css"
+  },
+  {
+    "revision": "040478cc212eb7dc37ce",
+    "url": "/static/css/main.ccfc2acb.chunk.css"
+  },
+  {
+    "revision": "9d76f39c62765e85400d",
+    "url": "/static/js/2.7dc0d3c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7dc0d3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040478cc212eb7dc37ce",
+    "url": "/static/js/main.41378ebe.chunk.js"
+  },
+  {
+    "revision": "0bb9ea73efab691cb07e",
+    "url": "/static/js/runtime-main.f5bbd9b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reservation/precache-manifest.a6157bf6903c73be3239e88bc3c1acff.js"
+  "/precache-manifest.f5f5daeca0ec8aceabf207c3dddb40a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reservation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
